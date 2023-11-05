d50d46d36ceeb13cb1dfd86d5babfa398ee4c511
J.H. Moon <moonjh587@naver.com>
2023-11-05 23:43:50 +0900

    [71st] PhoneViewController Rx적용해보기

--- SeSACRxThreads/SignUpOptional/PhoneViewController.swift
+++ SeSACRxThreads/SignUpOptional/PhoneViewController.swift
@@ -6,12 +6,22 @@
 //
  
 import UIKit
+
 import SnapKit
+import RxSwift
+import RxCocoa
 
 class PhoneViewController: UIViewController {
    
     let phoneTextField = SignTextField(placeholderText: "연락처를 입력해주세요")
     let nextButton = PointButton(title: "다음")
+    
+    let phone = BehaviorSubject(value: "010")
+    let buttonEnabled = BehaviorSubject(value: false)
+    let buttonColor = BehaviorSubject(value: UIColor.red)
+    
+    
+    let disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,6 +31,44 @@ class PhoneViewController: UIViewController {
         configureLayout()
         
         nextButton.addTarget(self, action: #selector(nextButtonClicked), for: .touchUpInside)
+        
+        bind()
+    }
+    
+    func bind(){
+        buttonColor
+            .map{ $0.cgColor }
+            .bind(to: phoneTextField.layer.rx.borderColor)
+            .disposed(by: disposeBag)
+        
+        buttonColor
+            .bind(to: nextButton.rx.backgroundColor, phoneTextField.rx.tintColor)
+            .disposed(by: disposeBag)
+        
+        buttonEnabled
+            .bind(to: nextButton.rx.isEnabled)
+            .disposed(by: disposeBag)
+        
+        phone
+            .bind(to: phoneTextField.rx.text)
+            .disposed(by: disposeBag)
+        
+        phone
+            .map{ $0.count > 12678 }
+            .subscribe(with: self) { owner, boolValue in
+                let color = boolValue ? UIColor.blue : UIColor.red
+                owner.buttonColor.onNext(color)
+                owner.buttonEnabled.on(.next(boolValue))
+            }
+            .disposed(by: disposeBag)
+        
+        phoneTextField
+            .rx.text.orEmpty
+            .subscribe(with: self) { owner, string in
+                let result = string.formated(by: "###-####-####")
+                owner.phone.on(.next(result))
+            }
+            .disposed(by: disposeBag)
     }
     
     @objc func nextButtonClicked() {
